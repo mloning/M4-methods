0be685ebac9c6ac9a41f5917dc66ddbf06996ac5
mloning <markus.loning.17@ucl.ac.uk>
2019-05-21 09:37:55 +0100

    updated benchmarking script and notebook

--- scripts/run_baselines.py
+++ scripts/run_baselines.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3 -u
 
+# preliminaries
 import numpy as np
 from tqdm import tqdm
 import pandas as pd
@@ -7,16 +8,15 @@ import os
 from sklearn.neural_network import MLPRegressor
 
 from copy import deepcopy
-from sktime.forecasting.forecasters import DummyForecaster
-from sktime.forecasting.forecasters import ExpSmoothingForecaster
+from sktime.forecasters import DummyForecaster
+from sktime.forecasters import ExpSmoothingForecaster
 from sktime.highlevel import Forecasting2TSRReductionStrategy
 from sktime.highlevel import ForecastingStrategy
 from sktime.highlevel import ForecastingTask
 
 
-#  Set paths
+# set paths
 home = os.path.expanduser("~")
-
 repodir = os.path.join(home, "Documents/Research/python_methods/m4-methods/")
 datadir = os.path.join(repodir, "Dataset")
 traindir = os.path.join(datadir, 'Train')
@@ -29,10 +29,11 @@ assert os.path.exists(traindir)
 assert os.path.exists(testdir)
 assert os.path.exists(savedir)
 
-#  Import meta data
+
+#  import meta data
 info = pd.read_csv(os.path.join(datadir, 'M4-info.csv'))
 
-# Get M4 baseline methods
+# get M4 baseline methods
 m4_results = pd.read_excel(os.path.join(repodir, 'Evaluation and Ranks.xlsx'),
                            sheet_name='Point Forecasts-Frequency',
                            header=[0, 1]).dropna(axis=0)
@@ -40,7 +41,8 @@ strategies = m4_results.loc[:, ['Method', 'User ID']].iloc[:, 0]
 baselines = [strategy for strategy in strategies if isinstance(strategy, str)]
 print('Baseline strategies:', baselines)
 
-# Define baseline forecasters
+
+# define strategies
 forecasters = {
     'Naive': DummyForecaster(strategy='last'),  # without seasonality adjustments
     #     'sNaive': None,
@@ -65,30 +67,37 @@ strategies.extend([ForecastingStrategy(estimator, name=name)
 strategies.extend([Forecasting2TSRReductionStrategy(estimator, name=name)
                    for name, estimator in regressors.items()])
 
+
+# select strategies
 selected_strategies = ('Naive',) # 'Naive2', 'SES', 'Holt', 'Damped')
 print('Selected strategies:', selected_strategies)
 
 strategies = [strategy for strategy in strategies
               if strategy.name in selected_strategies]
 
-#  Dictionary of forecasting horizons
+
+# dictionary of forecasting horizons
 fhs = info.set_index('SP')['Horizon'].to_dict()
 
-# Dictionary of frequencies
+
+# dictionary of seasonal periodicities
 freqs = info.set_index('SP')['Frequency'].to_dict()
 
-# Get dataset names
+
+# get dataset names
 files = os.listdir(os.path.join(traindir))
 datasets = [f.split('-')[0] for f in files]
 
-#  Select weekly dataset
-selected_datasets = ('Hourly', 'Daily', 'Weekly', 'Monthly', 'Yearly')
+
+# select datasets
+selected_datasets = ('Hourly', 'Daily', 'Weekly', 'Monthly', 'Quarterly', 'Yearly')
 print(selected_datasets)
+
 
 for dataset in selected_datasets:
     print(f"Dataset: {dataset}")
 
-    # Get forecasting horizon
+    # get forecasting horizon
     fh = np.arange(fhs[dataset]) + 1
     print('Forecasting horizon: ', len(fh))
 
@@ -100,40 +109,41 @@ for dataset in selected_datasets:
     alltrain = pd.read_csv(os.path.join(traindir, f'{dataset}-train.csv'), index_col=0)
     alltest = pd.read_csv(os.path.join(testdir, f'{dataset}-test.csv'), index_col=0)
 
-    # Get number of series in dataset
+    # get number of series in dataset
     n_series = alltrain.shape[0]
     assert n_series == info.SP.value_counts()[dataset]
 
-    # Iterate over strategies
+    # iterate over strategies
     for strategy in strategies:
-        # allocate output array
-        losses = np.zeros((n_series, 2))
 
-        # Iterate over series in dataset
+        # preallocate results matrix
+        y_preds = np.zeros((n_series, len(fh)))
+
+        # create strategy directory if necessary
+        filedir = os.path.join(savedir, strategy.name)
+        if not os.path.isdir(filedir):
+            os.makedirs(filedir)
+
+        # if results file already exists, skip series
+        filename = os.path.join(filedir, f"{strategy.name}_{dataset}_y_preds.txt")
+        if os.path.isfile(filename):
+            continue
+
+        # iterate over series in dataset
         for i in tqdm(range(n_series), desc=f"{strategy.name}", unit='series'):
 
-            # Dataset id
+            # dataset id
             dataset_id = f"{dataset[0]}{i + 1}"
 
-            # create strategy directory if necessary
-            filedir = os.path.join(savedir, strategy.name, dataset)
-            if not os.path.isdir(filedir):
-                os.makedirs(filedir)
-
-            # if results file already exists, skip series
-            filename = os.path.join(filedir, f"{strategy.name}_{dataset_id}_y_pred.txt")
-            if os.path.isfile(filename):
-                continue
-
-            # Get dataset
+            # get dataset
             y_train = alltrain.loc[dataset_id, :].dropna().reset_index(drop=True)
             y_test = alltest.loc[dataset_id, :].dropna().reset_index(drop=True)
             target_name = y_train.name
 
-            # Check forecasting horizon
+            # check forecasting horizon
             assert len(fh) == len(y_test)
 
-            #  get train data into expected format
+            # get train data into expected format
             train = pd.DataFrame(pd.Series([y_train]), columns=[target_name])
             n_obs = len(train.iloc[0, 0])
 
@@ -160,5 +170,7 @@ for dataset in selected_datasets:
             y_pred = s.predict()
             assert y_pred.index.equals(y_test.index)
 
-            # save predictions
-            np.savetxt(filename, y_pred.values)
+            y_preds[i, :] = y_pred.values
+
+        # save predictions
+        np.savetxt(filename, y_preds)
